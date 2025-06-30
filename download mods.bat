ded66cf4f27dc36f6b9587e6866551188a085e1e
FlashLight34 <joflash25@hotmail.com>
2025-06-30 00:30:33 -0400

    Add files via upload
    add md .\mods to create mod directory if not exist
    added filter to "snapshot" to not see him in json file

--- download mods.bat
+++ download mods.bat
@@ -10,17 +10,20 @@ set projectid_list=Xaeros_Minimap:263420 XaerosWorldMap:317780 fabric-api:306612
 set versionmc="1.21.6"
 set modapi="Fabric"
 
-SET BINDIR=%~dp0mods\
-CD /D "%BINDIR%"
-rem echo %BINDIR%
-set version=1.1
-
 rem start
+set version=1.2
 set title=Mods downloader by Flash v%version%
 title %title%
 echo [36m======================================[0m
 echo [36m==== [35m%title% [36m===[0m
 echo [36m======================================[0m
+SET BINDIR=%~dp0mods\
+rem create mods dir if not exist
+if not exist %BINDIR% (
+  echo [33mCréation du dossier mods.[0m
+  md .\mods
+)
+CD /D "%BINDIR%"
 
 rem add jq if not exist important to take infos in .json files
 jq -version > nul 2>&1
@@ -28,12 +31,12 @@ if not !errorlevel! == 2 (
   echo [33mInstallation du paquet jqlang.jq...[0m
   winget install jqlang.jq
   call :pause 1
-  echo [33mInstallation terminÃ©.[0m
+  echo [33mInstallation terminé.[0m
   call :pause 2
 )
 
 rem get old file and add it to see if already exist
-echo [33mVÃ©rification des fichiers actuel ...[0m
+echo [33mVérification des fichiers actuel ...[0m
 set "existingmodsfiles="
 for %%i in (*.jar) do (
   set "existingmodsfiles=!existingmodsfiles!%%i "
@@ -41,13 +44,13 @@ for %%i in (*.jar) do (
 rem count many mod
 set /a count=0
 for %%a in (%projectid_list%) do set /a count=!count!+1
-echo [33mIl y a[34m !count! [33mmods...[0m
+echo [33mIl y a[34m !count! [33mmods pour [34m!modapi:"=![33m et Minecraft version [34m!versionmc:"=![33m...[0m
 echo.
 call :pause 2
 set anynews=0
 rem loop each mod
 for %%a in (%projectid_list%) do (
-  echo [33mVÃ©rification des informations pour le mod: [34m%%a[0m
+  echo [33mVérification des informations pour le mod: [34m%%a[0m
   call :pause 2
   set "modname="
   set "modid="
@@ -56,7 +59,8 @@ for %%a in (%projectid_list%) do (
   set filemodinfos=modfile_!modname!.json
   rem download .json
   set url="https://www.curseforge.com/api/v1/mods/!modid!/files?pageIndex=0&pageSize=20&sort=dateCreated&sortDescending=true&removeAlphas=true&gameVersionTypeId=4"
-  curl -s --ssl-no-revoke -L !url! | jq -r ".data[] | select(.gameVersions | tostring | contains(\"!versionmc!\") and contains(\"!modapi!\"))" >!filemodinfos!
+  rem only take versionmc and modapi without snapshot
+  curl -s --ssl-no-revoke -L !url! | jq -r ".data[] | select(.gameVersions | tostring | contains(\"snapshot\") | not) | select(.gameVersions | tostring | contains(\"!versionmc!\") and contains(\"!modapi!\"))" >!filemodinfos!
   call :pause 1
   rem get infos from .json file with jqlang
   set "fileid="
@@ -114,7 +118,7 @@ set fi=%2
 set fn=%3
 set url="https://www.curseforge.com/api/v1/mods/%mi%/files/%fi%/download"
 set "fn=%fn:"=%"
-echo. [35mTÃ©lÃ©chargement du mod[33m %fn% [0m^([35mid:[33m %fi%[0m^)
+echo. [35mTéléchargement du mod[33m %fn% [0m^([35mid:[33m %fi%[0m^)
 curl -s --ssl-no-revoke -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -H "accept: application/json" -L %url% -o "%BINDIR%%fn%"
 EXIT /B 0
 :pause
